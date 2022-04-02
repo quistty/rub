a674df5a7c18effa67bab8960b67731cd4c3db69
quistty <78366933+quistty@users.noreply.github.com>
2022-04-02 12:51:58 -0400

    added the ability to use both require and import

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 
+// Define "require"
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+//gets the bot token code from a different file, so this repo can be uploaded to github
 let botTokenFile = require('../botToken.json');
 let botToken = botTokenFile["botToken"]
 
