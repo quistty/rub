ffc038bc2abcb34ddf6de9ead50effca70ba6168
quistty <78366933+quistty@users.noreply.github.com>
2022-08-12 12:43:52 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 //gets the bot token code from a different file, so this repo can be uploaded to github
-let botTokenFile = require('../botToken.json');
-let botToken = botTokenFile["botToken"]
+let botTokenFile = require("../botToken.json");
+let botToken = botTokenFile["botToken"];
 
 //allows us to use fetch. The old way does not work anymore
-import fetch from "node-fetch"; 
+import fetch from "node-fetch";
 import prompt from "prompt";
 
 //imports the apikey and stuff
@@ -18,63 +18,57 @@ import prompt from "prompt";
 prompt.start(); //starting prompt
 
 //importing discord stuff
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = '!';
-
+const prefix = "!";
 
 //                              code starts beneath
+let griffinAmount = 975;
+fetch(`https://api.hypixel.net/skyblock/bazaar`)
+  .then((response) => response.json())
+  .then((data) => {
+    //importing all the data from the hypixel api and turning it into variables we can use
+    let griffinInfo = data.products.GRIFFIN_FEATHER.quick_status.buyPrice;
+    let griffinInfoRounded = Math.round(griffinInfo);
+    console.log("Non-rounded Griffin Feather prices =", griffinInfo);
+    console.log("Rounded Griffin feather prices =", griffinInfoRounded);
+  })
+  .catch((error) =>
+    console.log("An error has occured, this is the messsage:", error)
+  ); // this is just to catch any errors
 
-fetch(`https://api.hypixel.net/skyblock/bazaar`) 
-    .then(response => response.json())
-    .then(data => {
-        //importing all the data from the hypixel api and turning it into variables we can use
-       let griffinInfo = data.products.GRIFFIN_FEATHER.quick_status.buyPrice
-       let griffinInfoRounded = Math.round(griffinInfo)
-        console.log("Non-rounded Griffin Feather prices =", griffinInfo)
-        console.log("Rounded Griffin feather prices =", griffinInfoRounded)
-    })
-    .catch(error => console.log("An error has occured, this is the messsage:", error)) // this is just to catch any errors
-
-
-        //asking and calculating how much profit you would make by selling at the current market rate
+//asking and calculating how much profit you would make by selling at the current market rate
 //        console.log(griffinInfoRounded *= griffinAmount)
 
-client.once('ready', () => {
-    console.log('aye rub is online')
+client.once("ready", () => {
+  console.log("aye rub is online");
 });
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if(command === 'ping'){
-        message.channel.send('pong!')
-    }
-    if(command === 'pong'){
-        message.channel.send('ping!')
-    }
-    if(command === 'griffin'){
-        message.reply(
-            'How many griffin feathers have you bought?'
-           );
-           // await the next message sent (in that channel) by the message author
-           message.channel
-            .awaitMessages((m) => m.author === message.author, { max: 1 })
-            .then((collected) => {
-                let griffinAmount = collected
-                var griffinProfit = griffinInfoRounded *= griffinAmount
-                message.reply(griffinProfit);
-            });
-    }
-
-
+  if (command === "ping") {
+    message.channel.send("pong!");
+  }
+  if (command === "pong") {
+    message.channel.send("ping!");
+  }
+  if (command === "griffin") {
+    message.reply("How many griffin feathers have you bought?");
+    // await the next message sent (in that channel) by the message author
+    message.channel
+      .awaitMessages((m) => m.author === message.author, { max: 1 })
+      .then((collected) => {
+        let griffinAmount = 975;
+        var griffinProfit = (griffinInfoRounded *= griffinAmount);
+        message.reply(griffinProfit);
+      });
+  }
 });
-
 
 client.login(botToken);
